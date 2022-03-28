c931979e3459a6ca5eafdbc4b22f2e9867a639c8
Marwan Ayoub <mayoub@c2r7p7.42nice.fr>
2022-03-28 18:04:06 +0200

    mini maj of le commit precedent

--- mlx_window.c
+++ mlx_window.c
@@ -6,7 +6,7 @@
 /*   By: mayoub <mayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 12:17:45 by mayoub            #+#    #+#             */
-/*   Updated: 2022/03/28 16:15:45 by mayoub           ###   ########.fr       */
+/*   Updated: 2022/03/28 18:03:30 by mayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,14 +27,13 @@ void	window(t_game *all)
 	printf("%d, %d\n", all->map.height, all->map.lengh);
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, x * 64, y * 64, "so_long");
-	while (i < 5)
+	while (i < 3)
 	{
 		img = mlx_xpm_file_to_image(mlx, "./sprites/ground.xpm", &x, &y);
 		mlx_put_image_to_window(mlx, win, img, x, y);
 		i++;
-		y *= 64;
-		x *= 64;
+		y += 64;
+		x += 64;
 	}
-	printf("%p\n", img);
 	mlx_loop(mlx);
 }
